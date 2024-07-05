f8084274b0050c6b8dad127281a9682e35347d39
foresec <foresec@naver.com>
2024-07-05 18:24:45 +0900

    ✨ feat: 딜레이 유무선택이 가능한 옵션 추가

--- src/useWindowSize.ts
+++ src/useWindowSize.ts
@@ -1,24 +1,36 @@
 import { useEffect, useState } from 'react';
 
-const useWindowSize = () => {
+const useWindowSize = ({ isDelay = true, delayTime = 200 }) => {
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
 
   useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
     const handleWindowResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
+      clearTimeout(timeoutId);
+      if (isDelay) {
+        timeoutId = setTimeout(() => {
+          setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+          });
+        }, delayTime);
+      } else {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
     };
 
     window.addEventListener('resize', handleWindowResize);
     return () => {
       window.removeEventListener('resize', handleWindowResize);
+      clearTimeout(timeoutId);
     };
-  });
+  }, [isDelay, delayTime]);
 
   return windowSize;
 };
