1667f2d2123b3050299b188275ebf44e27587bd0
foresec <foresec@naver.com>
2024-07-09 12:36:01 +0900

    💬 comment: 함수 params 변경에 따른 내용 수정

--- src/useWindowSize.ts
+++ src/useWindowSize.ts
@@ -6,12 +6,11 @@ interface WindowSize {
 }
 
 /**
- * useWindowSize : 브라우저 창의 너비와 높이를 제공하는 훅
- * @param {number} [delayTime=200] 지연 시간(밀리초). isDelay가 true일 때 사용됨.Default=200
- * @returns {{ width: number, height: number }} 창의 너비와 높이를 담은 객체
+ * useWindowSize : 브라우저의 너비와 높이를 제공하는 훅
+ * @param {number} [delayTime=200] 지연 시간(ms). 리사이즈 이벤트 딜레이 설정. Default=200
+ * @returns {{ width: number, height: number }} 브라우저의 너비와 높이를 담은 객체
  * @description
  * 브라우저 창의 사이즈가 변경될 때마다 해당 창의 너비와 높이를 업데이트합니다.
- * delay유무와 delayTime을 설정할 수 있습니다.
  * UI의 세밀한 조작 등 동적인 변화에 유용합니다.
  */
 
@@ -41,7 +40,7 @@ const useWindowSize = (delayTime: number = 200): WindowSize => {
       if (delayTime > 0) {
         timeoutId = setTimeout(updateWindowSize, delayTime);
       } else {
-        updateWindowSize;
+        updateWindowSize();
       }
     };
 
@@ -50,7 +49,6 @@ const useWindowSize = (delayTime: number = 200): WindowSize => {
       window.removeEventListener('resize', handleWindowResize);
       if (timeoutId) clearTimeout(timeoutId);
     };
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
