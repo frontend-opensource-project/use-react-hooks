ae901e69e27c4ea76963381cdbe8848615705fa7
bicochan <chanii8@naver.com>
2024-07-16 23:16:06 +0900

    ✨ feat: useConfirm 생성

--- src/context/ConfirmContext.tsx
+++ src/context/ConfirmContext.tsx
@@ -1,11 +1,15 @@
 import { createContext, useState } from 'react';
 
 interface ConfirmContext {
+  message?: string;
+  resolve?: (value: boolean) => void;
   setMessage: (message: string) => void;
   setResolve: (resolve: (value: boolean) => void) => void;
 }
 
 export const ConfirmContext = createContext<ConfirmContext>({
+  message: '',
+  resolve() {},
   setMessage() {},
   setResolve() {},
 });
@@ -15,29 +19,13 @@ export const ConfirmProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState<string>();
   const [resolve, setResolve] = useState<(value: boolean) => void>();
 
-  const onConfirm = () => {
-    resolve?.(true);
-  };
-
-  const onCancel = () => {
-    resolve?.(false);
-  };
-
   return (
-    <ConfirmContext.Provider value={{ setMessage, setResolve }}>
-      <>
-        {children}
-        <div className="dialog">
-          <div className="dialog-content">{message}</div>
-          <div className="dialog-actions">
-            <button onClick={onCancel}>아니오</button>
-            <button onClick={onConfirm}>예</button>
-          </div>
-        </div>
-      </>
+    <ConfirmContext.Provider
+      value={{ message, resolve, setMessage, setResolve }}>
+      {children}
     </ConfirmContext.Provider>
   );
 };
--- src/hooks/useConfirm.ts
+++ src/hooks/useConfirm.ts
@@ -2,17 +2,28 @@ import { useContext } from 'react';
 import { ConfirmContext as context } from '../context/ConfirmContext';
 
 const useConfirm = () => {
-  const { setMessage, setResolve } = useContext(context);
+  const { message, resolve, setMessage, setResolve } = useContext(context);
+
+  const isOpen = !!message;
 
   const confirm = (message: string) => {
     setMessage(message);
-
     return new Promise<boolean>((resolve) => {
       setResolve(() => resolve);
     });
   };
 
-  return confirm;
+  const onConfirm = () => {
+    resolve?.(true);
+    setMessage('');
+  };
+
+  const onCancel = () => {
+    resolve?.(false);
+    setMessage('');
+  };
+
+  return { message, isOpen, confirm, onConfirm, onCancel };
 };
 
 export default useConfirm;
