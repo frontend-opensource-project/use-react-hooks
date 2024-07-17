a4541304ba5b1a224854f54e9b29070b87c9b568
bicochan <chanii8@naver.com>
2024-07-17 20:28:44 +0900

    🩹 chore: config 파일 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'src/script/githubEventHandler.ts',
+      output: {
+        entryFileNames: 'script/[name].js',
+        chunkFileNames: 'script/[name].js',
+        assetFileNames: 'script/[name].[ext]',
+      },
+    },
+  },
+});
