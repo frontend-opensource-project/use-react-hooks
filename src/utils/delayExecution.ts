77ee9281c3c408fe066d66f33dfdb5fe2ed2f0d4
jeongbaebang <jeongbaebang_dev@naver.com>
2024-07-25 22:50:03 +0900

    ✨ feat: 예외 케이스 추가

--- src/utils/delayExecution.ts
+++ src/utils/delayExecution.ts
@@ -2,19 +2,34 @@ export interface CancelToken {
   isCancelled: boolean;
 }
 
+type Status = 'started' | 'cancelled';
+
 export const delayExecution = (ms: number) => {
   let timeoutId: NodeJS.Timeout | undefined;
   const defaultCancelToken: CancelToken = { isCancelled: false };
 
   const startHandler = (cancelToken: CancelToken = defaultCancelToken) => {
-    return new Promise<void>((resolve) => {
-      timeoutId = setTimeout(() => {
-        if (!cancelToken.isCancelled) {
-          resolve();
-        } else {
-          clearHandler();
-        }
-      }, ms);
+    return new Promise<Status>((resolve, reject) => {
+      if (!cancelToken || typeof cancelToken.isCancelled !== 'boolean') {
+        reject(new ReferenceError('Invalid cancel token provided'));
+      }
+
+      if (timeoutId !== undefined) {
+        reject(new Error('Previous timeout is still pending'));
+      }
+
+      try {
+        timeoutId = setTimeout(() => {
+          if (!cancelToken.isCancelled) {
+            resolve('started');
+          } else {
+            clearHandler();
+            resolve('cancelled'); // Promise를 resolve하여, 대기 상태에서 벗어나게 처리
+          }
+        }, ms);
+      } catch (error) {
+        reject(new Error('Failed to set a timeout'));
+      }
     });
   };
 
