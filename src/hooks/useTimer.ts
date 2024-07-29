ffc5d8f0192fc5ac69e95390784457bc0b405a6f
jeongbaebang <jeongbaebang_dev@naver.com>
2024-07-29 09:38:37 +0900

    📝 docs: useTimer 문서 주석 추가

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -5,6 +5,23 @@ import useUnmountEffect from './useUnmountEffect';
 
 type Callback = () => void;
 
+/**
+ * 일정 시간(ms) 후에 지정된 콜백 함수를 실행하는 타이머 훅.
+ *
+ * @param {function} callback 타이머가 완료된 후 실행할 콜백 함수.
+ * @param {number} ms 지연 시간(밀리초). 양의 정수로 지정.
+ *
+ * @returns {object}
+ *  - `start`: 타이머를 시작하는 함수.
+ *  - `cancel`: 현재 활성화된 타이머를 취소하는 함수.
+ *
+ * @description
+ * - 이 훅은 주어진 시간(ms)이 지난 후 콜백 함수를 실행하는 타이머를 제공합니다.
+ * - `start` 함수를 호출하면 타이머가 시작되며, 지정된 시간이 지나면 콜백 함수가 호출됩니다.
+ * - `cancel` 함수를 호출하면 현재 활성화된 타이머를 취소할 수 있습니다.
+ * - 콜백 함수가 변경될 때마다 참조를 업데이트합니다.
+ * - 컴포넌트가 언마운트될 때 타이머를 정리합니다.
+ */
 const useTimer = (callback: Callback, ms: number) => {
   const callbackRef = useRef<Callback>(callback);
   const timerRef = useRef<ReturnType<typeof delayExecution> | null>(null);
