b15a968ad56a6be08bcb42666be4f3ef7eef742f
foresec <foresec@naver.com>
2024-08-01 03:11:47 +0900

    💬 comment: useDetectInactivity jsdocs작성

--- src/hooks/useDetectInactivity.ts
+++ src/hooks/useDetectInactivity.ts
@@ -2,6 +2,19 @@ import { useCallback, useEffect, useState } from 'react';
 import useTimer from './useTimer';
 import { Fn } from '../types';
 
+/**
+ * 일정 시간(ms) 동안 활동이 없을 때 지정된 콜백 함수를 실행하는 훅.
+ *
+ * @param {number} time - 비활성 상태로 간주되기까지의 시간(밀리초). 양의 정수로 지정.
+ * @param {Fn} onInactivity - 비활성 상태가 감지되었을 때 호출되는 콜백 함수.
+ *
+ * @returns {boolean} - 현재 비활동 상태 여부를 나타내는 boolean 값.
+ *
+ * @description
+ * 사용자가 정의한 시간(time) 동안 활동이 없으면 비활성 상태로 간주하고, 지정된 콜백 함수(onInactivity)를 호출합니다.
+ * 해당 환경에 맞게 설정된 이벤트를 리스닝하여, 활동이 감지될 시 타이머를 리셋합니다.
+ */
+
 const useDetectInactivity = (time: number, onInactivity: Fn) => {
   const [isInactive, setIsInactive] = useState(false);
   const { start } = useTimer(() => setIsInactive(true), time);
