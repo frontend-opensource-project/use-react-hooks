d4878eb4e05efb3b689391572122362613f37c00
suhyeoonn <suhyeon1740@gmail.com>
2024-08-06 10:30:54 +0900

    📝 docs: useLongPress JSDoc 작성

--- src/hooks/useLongPress.ts
+++ src/hooks/useLongPress.ts
@@ -1,6 +1,17 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { Fn } from '../types';
 
+/**
+ * 사용자가 요소를 길게 눌렀을 때 콜백 함수를 호출하는 훅
+ *
+ * @param {Function} callback - 사용자가 요소를 길게 눌렀을 때 호출될 콜백 함수
+ * @param {number} delay - (옵션) 사용자가 요소를 길게 눌러야 하는 시간(밀리초, 기본값 500ms)
+ *
+ * @returns {Object} { onMouseDown, onMouseUp, onMouseLeave, onTouchStart, onTouchEnd } - 요소에 연결될 이벤트 핸들러 객체:
+ *
+ * @description
+ * 사용자가 지정한 시간 이상 요소를 눌렀을 때 콜백 함수가 호출되며, 마우스와 터치 이벤트를 모두 지원합니다.
+ */
 const useLongPress = (callback: Fn, delay = 500) => {
   const [isPress, setIsPress] = useState(false);
   const timeoutRef = useRef<NodeJS.Timeout | null>(null);
