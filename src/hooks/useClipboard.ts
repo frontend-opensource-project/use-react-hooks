7d8ea9e086129302a42910fe19aa103c82408cb4
bicochan <chanii8@naver.com>
2024-08-12 00:37:46 +0900

    📝 docs: jsdoc 추가

--- src/hooks/useClipboard.ts
+++ src/hooks/useClipboard.ts
@@ -1,6 +1,16 @@
 import { useState, useEffect } from 'react';
 import { imgToBlob } from '../utils';
 
+/**
+ * 클립보드에 텍스트나 이미지를 복사하는 커스텀 훅
+ *
+ * @returns {boolean} `copied`: 복사 작업의 성공 여부를 나타내는 플래그
+ * @returns {(string) => Promise<void>} `copyText`: 텍스트를 클립보드에 복사하는 비동기 함수
+ * @returns {(string) => void} `copyImg`: 주어진 경로의 이미지를 클립보드에 복사하는 함수
+ *
+ * @description
+ * 클립보드 API가 지원되지 않는 경우 에러를 발생시킵니다.
+ */
 const useClipboard = () => {
   const [copied, setCopied] = useState(false);
 
