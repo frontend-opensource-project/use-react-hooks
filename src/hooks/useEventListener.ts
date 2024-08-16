27c9d2fd87665ab2d8bc5059e9070e60a7918d4c
foresec <foresec@naver.com>
2024-08-16 16:26:04 +0900

    ✨ feat: 여러가지 event type 확장

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -1,27 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
-// window event
-// dom elements
 // document
 // mediaqueries
 // with ref
 
-interface useEventListenerProps<K extends keyof WindowEventMap> {
+// isClient해줘야하나?
+
+type EventMap =
+  | WindowEventMap
+  | DocumentEventMap
+  | ElementEventMap
+  | SVGElementEventMap
+  | MediaQueryListEventMap;
+
+interface useEventListenerProps<K extends keyof EventMap> {
   eventName: K;
-  handler: (event: WindowEventMap[K]) => void;
-  element?: Window | Document | Element | null;
+  handler: (event: EventMap[K]) => void;
+  element?:
+    | Window
+    | Document
+    | HTMLElement
+    | HTMLElement
+    | SVGElement
+    | MediaQueryList
+    | null;
 }
 
-function useEventListener<K extends keyof WindowEventMap>({
+function useEventListener<K extends keyof EventMap>({
   eventName,
   handler,
   element = window,
 }: useEventListenerProps<K>) {
+  const savedHandler = useRef(handler);
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
   useEffect(() => {
     if (!element) return;
 
     const eventListener = (event: Event) => {
-      handler(event as WindowEventMap[K]);
+      handler(event as EventMap[K]);
     };
 
     element.addEventListener(eventName, eventListener);
