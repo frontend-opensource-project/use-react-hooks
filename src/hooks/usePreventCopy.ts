be570d8a2fc52f6e880b3124dacbf77b98b5be89
bicochan <chanii8@naver.com>
2024-08-18 01:32:14 +0900

    ✨ feat: usePreventCopy 신규 생성

---
+++ src/hooks/usePreventCopy.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+/**
+ * 브라우저에서 복사 이벤트를 차단하는 훅
+ *
+ * @param {VoidFunction} callback - 복사 이벤트가 발생할 때 실행될 콜백 함수
+ *
+ * @description 복사 이벤트를 차단하고 콜백 함수를 실행합니다.
+ */
+const usePreventCopy = (callback?: () => void) => {
+  const preventCopy = (e: ClipboardEvent) => {
+    e.preventDefault();
+    callback?.();
+  };
+
+  useEffect(() => {
+    // TODO: useEventListener 적용하기
+    window.addEventListener('copy', preventCopy);
+    return () => {
+      window.removeEventListener('copy', preventCopy);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
+
+export default usePreventCopy;
