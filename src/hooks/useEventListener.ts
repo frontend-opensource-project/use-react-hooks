1d326cef9b81b6e71ad9571fcf5d9433ac4547f8
foresec <foresec@naver.com>
2024-08-18 23:41:50 +0900

    📝 docs: useEventListener jsdocs 추가

--- src/hooks/useEventListener.ts
+++ src/hooks/useEventListener.ts
@@ -15,6 +15,19 @@ type EventElement = {
   svgElement: RefObject<SVGElement> | SVGElement;
 };
 
+/**
+ * 특정 이벤트에 대한 이벤트 리스너를 추가하여, 이벤트 발생 시 핸들러를 실행하는 훅.
+ *
+ * @param {E} eventName - 추가할 이벤트의 이름.
+ * @param {(event: EventMap[K][E]) => void} handler - 이벤트 발생 시 호출되는 콜백 함수.
+ * @param {EventElement[K] | null} [element=window] - 이벤트를 추가할 대상 Element, 기본값은 window.
+ * @param {AddEventListenerOptions} [options] - 이벤트 리스너 기본 옵션.
+ *
+ * @description
+ * 지정된 이벤트가 발생할 때마다 제공된 핸들러를 호출합니다.
+ * 가능한 Element로 window, document, htmlElement, svgElement
+ */
+
 function useEventListener<
   K extends keyof EventMap,
   E extends keyof EventMap[K] & string,
