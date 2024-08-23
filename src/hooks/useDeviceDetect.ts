8d132fb0f5666eac550d5b38273b94f722b2fab7
foresec <foresec@naver.com>
2024-08-23 20:20:10 +0900

    ✨ feat: os와 browser 추가

--- src/hooks/useDeviceDetect.ts
+++ src/hooks/useDeviceDetect.ts
@@ -2,31 +2,59 @@ import { useEffect, useState } from 'react';
 import { isClient } from '../utils';
 
 const useDeviceDetect = () => {
-  const [deviceType, setDeviceType] = useState({
+  const [deviceInfo, setDeviceInfo] = useState({
     isMobile: false,
     isTablet: false,
     isDesktop: false,
+    os: '',
+    browser: '',
   });
+
+  const detectDeviceType = (userAgent: string) => {
+    const isMobile = /Mobi/i.test(userAgent);
+    const isTablet = /Tablet|iPad/i.test(userAgent);
+    const isDesktop = !isMobile && !isTablet;
+    return { isMobile, isTablet, isDesktop };
+  };
+
+  const detectOS = (userAgent: string) => {
+    if (/Windows/i.test(userAgent)) return 'Windows';
+    if (/Macintosh|Mac/i.test(userAgent)) return 'MacOS';
+    if (/Linux/i.test(userAgent)) return 'Linux';
+    if (/Android/i.test(userAgent)) return 'Android';
+    if (/iOS|iPhone|iPad|iPod/i.test(userAgent)) return 'iOS';
+    return '';
+  };
+
+  const detectBrowser = (userAgent: string): string => {
+    if (/Edg/i.test(userAgent)) return 'Edge';
+    if (/Chrome/i.test(userAgent) && !/Edg/.test(userAgent)) return 'Chrome';
+    if (/Safari/i.test(userAgent) && !/Chrome/.test(userAgent)) return 'Safari';
+    if (/Firefox/i.test(userAgent)) return 'Firefox';
+    return '';
+  };
 
   useEffect(() => {
     if (!isClient) return;
 
     const UA = navigator.userAgent;
 
-    const isMobile = /Mobi/i.test(UA);
-    const isTablet = /Tablet|iPad/i.test(UA);
-    const isDesktop = !isMobile && !isTablet;
+    const { isMobile, isTablet, isDesktop } = detectDeviceType(UA);
+    const os = detectOS(UA);
+    const browser = detectBrowser(UA);
 
-    setDeviceType({
+    setDeviceInfo({
       isMobile,
       isTablet,
       isDesktop,
+      os,
+      browser,
     });
   }, []);
 
-  const { isMobile, isTablet, isDesktop } = deviceType;
+  const { isMobile, isTablet, isDesktop, os, browser } = deviceInfo;
 
-  return { isMobile, isTablet, isDesktop };
+  return { isMobile, isTablet, isDesktop, os, browser };
 };
 
 export default useDeviceDetect;
