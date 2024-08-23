e1d2a4e3636e5a605cc177951ba9ecd490e905b2
foresec <foresec@naver.com>
2024-08-23 20:57:52 +0900

    🩹 chore: app.tsx 기타코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import useDeviceDetect from './hooks/useDeviceDetect';
-
 function App() {
-  const { isMobile, isTablet, isDesktop, os, browser } = useDeviceDetect();
-  console.log(isMobile, isTablet, isDesktop, os, browser);
   return (
     <div>
       <h1>USE-REACT-HOOKS</h1>
