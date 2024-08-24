7124f2247f773a9c33871dd60208e928693d8d11
bicochan <chanii8@naver.com>
2024-08-25 02:41:00 +0900

    ✨ feat: useDeepCompareEffect 신규 생성

---
+++ src/hooks/useDeepCompareEffect.ts
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from 'react';
+import isEqual from 'fast-deep-equal';
+
+/**
+ * 의존성 배열의 깊은 비교를 통해 변화를 감지하고 콜백을 실행시키는 훅
+ *
+ * @param callback 의존성 배열의 변화가 감지되었을 때 실행할 콜백 함수
+ * @param dependencies 의존성 배열
+ */
+const useDeepCompareEffect = (
+  callback: () => void,
+  dependencies: unknown[]
+) => {
+  const prevDependencies = useRef(dependencies);
+
+  if (isEqual(dependencies, prevDependencies.current)) return;
+
+  prevDependencies.current = dependencies;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps, react-hooks/rules-of-hooks
+  useEffect(callback, prevDependencies.current);
+};
+
+export default useDeepCompareEffect;
