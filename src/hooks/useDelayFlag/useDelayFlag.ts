361386609e3f62c3ffa83480e6b616f064d6de9b
bicochan <chanii8@naver.com>
2024-09-29 00:50:07 +0900

    📝 docs: useDelayFlag 문서 수정

--- src/hooks/useDelayFlag/useDelayFlag.ts
+++ src/hooks/useDelayFlag/useDelayFlag.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { UseDelayFlagProps, UseDelayFlagReturns } from './type';
 /**
- * 플래그를 원하는 시간만큼 지연시킨 후에 업데이트하는 커스텀 훅
+ * 플래그를 원하는 시간만큼 지연시킨 후에 업데이트하는 훅
  *
  * @param {boolean} flag 지연시킬 플래그
  * @param {number} [delayTime=1000] 지연 시간(ms, 양의 정수)
@@ -9,7 +9,8 @@ import { UseDelayFlagProps, UseDelayFlagReturns } from './type';
  * @returns {boolean} delayTime 만큼 지연시킨 후에 업데이트된 flag 반환
  *
  * @description
- * 데이터 페칭 시, 페칭(또는 로딩) 플래그를 지정한 시간동안 유지하여 로딩 UI의 깜빡임 현상 제거, 중복 호출 방지 등의 용도로 사용할 수 있습니다.
+ * - 데이터 페칭 시, 페칭(또는 로딩) 플래그를 지연시킨 시간동안 유지하여 로딩 UI의 깜빡임 현상 제거, 중복 호출 방지 등의 용도로 사용할 수 있습니다.
+ * - 지연시킨 시간보다 플래그의 업데이트가 더 늦을 경우 지연 시간을 무시하고 플래그의 실제 업데이트 시점을 반환합니다.
  */
 const useDelayFlag = <T extends number>({
   flag,
