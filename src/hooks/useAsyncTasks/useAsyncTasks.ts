9c5fc0e84564327daf7e915a419216aa4e6ec7a4
jeongbaebang <jeongbaebang_dev@naver.com>
2024-10-11 21:41:41 +0900

    📝 docs: useAsyncTasks JSDOC 업데이트

--- src/hooks/useAsyncTasks/useAsyncTasks.ts
+++ src/hooks/useAsyncTasks/useAsyncTasks.ts
@@ -6,14 +6,26 @@ import {
   PromiseCircularityError,
 } from 'async-wave';
 import { delayExecution } from '@/utils';
+
 import { Task, Options, StateInfo, TaskAction, ACTION_TYPES } from './type';
 import { reducer } from './reducer';
 
 /**
- * useAsyncTasks
+ * useAsyncTasks: 비동기 작업 리스트와 옵션을 받아 상태 정보를 관리하는 훅
+ *
  * @param {Task<R>[]} tasks - 비동기 작업 리스트
- * @param {Options<R>} options - 비동기 작업 옵션
+ *   - 실행할 비동기 작업들을 담은 배열입니다.
+ * @param {Options<R>} [options={}] - 비동기 작업 옵션
+ *   - 비동기 작업 실행 중 설정할 옵션. 선택적이며 기본값은 빈 객체입니다.
+ *
  * @returns {StateInfo<R>} 비동기 작업 상태 정보를 반환
+ *   - 작업의 로딩 상태, 데이터, 오류 정보를 포함합니다.
+ *   - 추가적으로 `isError` 속성을 통해 오류 여부를 확인할 수 있고, `reset` 메서드를 통해 상태를 초기화할 수 있습니다.
+ *
+ * @description
+ * - 비동기 작업을 관리하며, 작업의 상태 정보(로딩, 데이터, 오류)를 제공합니다.
+ * - 컴포넌트 언마운트 시, 비동기 작업이 완료된 후 상태 업데이트를 방지합니다.
+ * - `reset` 메서드를 호출하여 상태를 초기화할 수 있습니다.
  */
 const useAsyncTasks = <R>(tasks: Task<R>[], options: Options<R>) => {
   const isMountedRef = useRef(false); // 컴포넌트가 언마운트된 후 비동기 작업이 완료될 때 상태 업데이트를 방지
