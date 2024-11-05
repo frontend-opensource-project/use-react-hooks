95ad851ffe247488b4f5bc82b2fd6e915b655214
박종찬 <chanii8@naver.com>
2024-11-05 14:44:00 +0900

    Update next.config.mjs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -2,8 +2,12 @@ import nextra from 'nextra';
 
 const withNextra = nextra('nextra-theme-docs', './theme.config.js');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default withNextra({
   output: 'export',
+  basePath: isProd ? '/use-react-hooks' : '',
+  assetPrefix: isProd ? '/use-react-hooks/' : '',
   images: {
     unoptimized: true,
   },
